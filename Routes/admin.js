153c766f2b3f920910917060c2d3c3da911a6d46
Darsh kashyap <dkdarshkashyap07@gmail.com>
2026-03-10 17:01:42 +0530

    added functionality to user and admin signup signin

--- Routes/admin.js
+++ Routes/admin.js
@@ -1,13 +1,29 @@
 const { Router } = require('express');
 const adminRouter = Router();
 const { adminModel } = require("../db.js");
-
-adminRouter.post("/signup", (req, res) => {
+const jwt=require("jsonwebtoken");
+const JWT_ADMIN_PASSWORD="dabestadmin";
+adminRouter.post("/signup", async function(req, res){
+const {email, password,firstName,lastName} = req.body;
+await adminModel.create({
+    email,
+    password,
+    firstName,  
+    lastName,
+});
+res.status(201).json({message: "Admin created successfully"});
 
 });
 
-adminRouter.post("/signin", (req, res) => {
-
+adminRouter.post("/signin", async function(req, res) {
+  const { email, password } = req.body;
+    const user = await adminModel.findOne({ email: email, password: password });
+    if(user) {
+       const token = jwt.sign({id:user._id}, JWT_ADMIN_PASSWORD, { expiresIn: "1h" });
+         res.json({message: "Admin signed in successfully", token: token});
+    } else {
+        res.status(401).json({message: "Invalid email or password"});
+    }
 });
 
 adminRouter.post("/courses", (req, res) => {
--- Routes/user.js
+++ Routes/user.js
@@ -1,15 +1,34 @@
 const { Router } = require('express');
 const userRouter = Router();
+const jwt=require("jsonwebtoken");
+const JWT_USER_PASSWORD="dabestuser";
+const { userModel } = require("../db");
 
-userRouter.post("/signup", (req, res) => {
+userRouter.post("/signup",async function(req, res){
+const { firstName,lastName, email, password } = req.body;
+
+await userModel.create({
+    firstName,
+    lastName,
+    email,
+    password
+});
+res.status(201).json({message: "User created successfully"});
 
 });
 
-userRouter.post("/signin", (req, res) => {
-
+userRouter.post("/signin", async function(req, res) {
+    const { email, password } = req.body;
+    const user = await userModel.findOne({ email: email, password: password });
+    if(user) {
+       const token = jwt.sign({id:user._id}, JWT_USER_PASSWORD, { expiresIn: "1h" });
+         res.json({message: "User signed in successfully", token: token});
+    } else {
+        res.status(401).json({message: "Invalid email or password"});
+    }
 });
 
 userRouter.get("/purchases", (req, res) => {
 
 });
-module.exports = {userRouter: userRouter};
+module.exports = {userRouter: userRouter};      
